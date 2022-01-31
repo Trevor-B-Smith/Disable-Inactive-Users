f6f3fe3019901760b064acd149ff9b6c1bf40802
Trevor Smith <trevor.smith@is4s.com>
2022-01-31 13:35:42 -0600

    finished script

--- Disable-90-Days.ps1
+++ Disable-90-Days.ps1
@@ -1,9 +1,24 @@
-$LocalUsers=(Get-LocalUser | select name,lastlogon)
+$LocalUsers=(Get-LocalUser | select name,lastlogon,enabled)
 $LocalUsers = $LocalUsers -split "} @{"
 $LocalUsers = $LocalUsers -replace '[{@}]',''
 
 foreach ($User in $LocalUsers) {
     $UserInfo = $User -split "; "
+    $Name = $UserInfo[0] -replace ".*=" 
+    $LastLogon = $UserInfo[1] -replace ".*=" 
+    $Enabled = $UserInfo[2] -replace ".*="
     Write-Host($UserInfo)
+    
+    if ($LastLogon.Length -gt 0){
+        $LastLogon = [datetime]$LastLogon
+        $DateLimit = (Get-Date).AddDays(-90)
 
+        #Checks date and if account is disabled
+        if ($LastLogon -lt $DateLimit -and $Enabled -eq "True"){
+            #If account is enabled & older than 90 days, disable it.
+            Write-Host("Disabled local user account " + $Name)
+            # Disable-LocalUser -Name $Name
+        }
+    }
 }
+
